f33a246c5063fba032ad812828a5c2ed2eee7d55
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-03-03 20:15:23 +0000

    Added how to fix ssl error in case it pops up

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 def handle_exception(exception_type, exception, stack_trace=None):
     exception_dict = {
         "EnvironmentError": f'Environment variable "{exception}" is not set.',
-        "SSLError": f'SSL verification failed. IGNORE_SSL_ERRORS is {exception}. EXITING.',
+        "SSLError": f'SSL verification failed. IGNORE_SSL_ERRORS is {exception}. Set IGNORE_SSL_ERRORS to True if you want to ignore this error. EXITING.',
         "GitCommandError": f'The repo "{exception}" is not a valid git repo.',
         "GitInvalidRepositoryError": f'The repo "{exception}" is not a valid git repo.',
         "Exception": f'An unknown error occurred: "{exception}"'
