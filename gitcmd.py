4fca4a7fb4d703ac80c99bc73c79bfc41a18937d
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-03-03 16:03:17 -0500

    Cleanup (#75)
    * - Added exception handler function with easy map dictionary.
    - Added extra default values for ENV vars.
    - Moved parser args to Settings for global use.
    - Started implementing new exception handler
    - Moved git functions to gitcmd.py.
    - Implemented exception handler for git functions.
    - Removed extra imports where no longer needed.
    
    * Added how to fix ssl error in case it pops up
    
    * - Removed circular import of settings.py and gitcmd.py
    - Added exception_handler.py to handle exceptions (refer to above)
    - Made GitCMD a class with init and run methods
    - Removed git completely from nb-dt-import.py
    
    * Fixed missing arg
    
    * made exception handler a class to prevent circular import

---
+++ gitcmd.py
@@ -0,0 +1,46 @@
+from git import Repo, exc
+from sys import exit as system_exit
+import settings
+import os
+
+class GitCMD:
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+        
+    def __init__(self, args, repo_path):
+        self.url = args.url
+        self.repo_path = repo_path
+        self.branch = args.branch
+        self.repo = Repo()
+        self.cwd = os.getcwd()
+        
+        if os.path.isdir(self.repo_path):
+            self.pull_repo()
+        else:
+            self.clone_repo()
+            
+        
+    def pull_repo(self):
+        try:
+            print("Package devicetype-library is already installed, "
+                    + f"updating {os.path.join(self.cwd, self.repo_path)}")
+            self.repo = Repo(self.repo_path)
+            if not self.repo.remotes.origin.url.endswith('.git'):
+                settings.handle.exception("GitInvalidRepositoryError", self.repo.remotes.origin.url, f"Origin URL {self.repo.remotes.origin.url} does not end with .git")
+            self.repo.remotes.origin.pull()
+            self.repo.git.checkout(self.branch)
+            print(f"Pulled Repo {self.repo.remotes.origin.url}")
+        except exc.GitCommandError as git_error:
+            settings.handle.exception("GitCommandError", self.repo.remotes.origin.url, git_error)
+        except Exception as git_error:
+            settings.handle.exception("Exception", 'Git Repository Error', git_error)
+        
+    def clone_repo(self):
+        try:
+            self.repo = Repo.clone_from(self.url, os.path.join(self.cwd, self.repo_path), branch=self.branch)
+            print(f"Package Installed {self.repo.remotes.origin.url}")
+        except exc.GitCommandError as git_error:
+            settings.handle.exception("GitCommandError", self.url, git_error)
+        except Exception as git_error:
+            settings.handle.exception("Exception", 'Git Repository Error', git_error)
+
--- settings.py
+++ settings.py
@@ -1,12 +1,18 @@
+from argparse import ArgumentParser
+from sys import exit as system_exit
 import os
+from exception_handler import ExceptionHandler
+from gitcmd import GitCMD
 from dotenv import load_dotenv
 load_dotenv()
 
-REPO_URL = os.getenv("REPO_URL")
-REPO_BRANCH = os.getenv("REPO_BRANCH", "master")
+REPO_URL = os.getenv("REPO_URL",
+                     default="https://github.com/netbox-community/devicetype-library.git")
+REPO_BRANCH = os.getenv("REPO_BRANCH", default="master")
 NETBOX_URL = os.getenv("NETBOX_URL")
 NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
-IGNORE_SSL_ERRORS = (os.getenv("IGNORE_SSL_ERRORS", "False") == "True")
+IGNORE_SSL_ERRORS = (os.getenv("IGNORE_SSL_ERRORS", default="False") == "True")
+REPO_PATH = "./repo"
 
 # optionally load vendors through a comma separated list as env var
 VENDORS = list(filter(None, os.getenv("VENDORS", "").split(",")))
@@ -18,8 +24,25 @@ NETBOX_FEATURES = {
     'modules': False,
 }
 
-MANDATORY_ENV_VARS = ["REPO_URL", "NETBOX_URL", "NETBOX_TOKEN"]
+parser = ArgumentParser(description='Import Netbox Device Types')
+parser.add_argument('--vendors', nargs='+', default=VENDORS,
+                    help="List of vendors to import eg. apc cisco")
+parser.add_argument('--url', '--git', default=REPO_URL,
+                    help="Git URL with valid Device Type YAML files")
+parser.add_argument('--slugs', nargs='+', default=SLUGS,
+                    help="List of device-type slugs to import eg. ap4431 ws-c3850-24t-l")
+parser.add_argument('--branch', default=REPO_BRANCH,
+                    help="Git branch to use from repo")
+parser.add_argument('--verbose', action='store_true', default=False,
+                    help="Print verbose output")
 
+args = parser.parse_args()
+
+handle = ExceptionHandler(args)
+# Evaluate environment variables and exit if one of the mandatory ones are not set
+MANDATORY_ENV_VARS = ["REPO_URL", "NETBOX_URL", "NETBOX_TOKEN"]
 for var in MANDATORY_ENV_VARS:
     if var not in os.environ:
-        raise EnvironmentError("Failed because {} is not set.".format(var))
+        handle.exception("EnvironmentError", var, f'Environment variable "{var}" is not set.\n\nMANDATORY_ENV_VARS: {str(MANDATORY_ENV_VARS)}.\n\nCURRENT_ENV_VARS: {str(os.environ)}')
+
+git_repo = GitCMD(args, REPO_PATH)
