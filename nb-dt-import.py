b9984534d72057df90c927e0e51933a71da83aff
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-03-09 05:14:28 +0000

    formatting main file

--- nb-dt-import.py
+++ nb-dt-import.py
@@ -9,12 +9,14 @@ import os
 import settings
 from netbox_api import NetBox
 
+
 def main():
     startTime = datetime.now()
     args = settings.args
 
     netbox = NetBox(settings)
-    files, vendors = settings.dtl_repo.get_devices(f'{settings.dtl_repo.repo_path}/device-types/', args.vendors)
+    files, vendors = settings.dtl_repo.get_devices(
+        f'{settings.dtl_repo.repo_path}/device-types/', args.vendors)
 
     settings.handle.log(f'{len(vendors)} Vendors Found')
     device_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
@@ -24,7 +26,8 @@ def main():
 
     if netbox.modules:
         settings.handle.log("Modules Enabled. Creating Modules...")
-        files, vendors = settings.dtl_repo.get_devices(f'{settings.dtl_repo.repo_path}/module-types/', args.vendors)
+        files, vendors = settings.dtl_repo.get_devices(
+            f'{settings.dtl_repo.repo_path}/module-types/', args.vendors)
         settings.handle.log(f'{len(vendors)} Module Vendors Found')
         module_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
         settings.handle.log(f'{len(module_types)} Module-Types Found')
@@ -32,13 +35,19 @@ def main():
         netbox.create_module_types(module_types)
 
     settings.handle.log('---')
-    settings.handle.verbose_log(f'Script took {(datetime.now() - startTime)} to run')
+    settings.handle.verbose_log(
+        f'Script took {(datetime.now() - startTime)} to run')
     settings.handle.log(f'{netbox.counter["added"]} devices created')
-    settings.handle.log(f'{netbox.counter["updated"]} interfaces/ports updated')
-    settings.handle.log(f'{netbox.counter["manufacturer"]} manufacturers created')
+    settings.handle.log(
+        f'{netbox.counter["updated"]} interfaces/ports updated')
+    settings.handle.log(
+        f'{netbox.counter["manufacturer"]} manufacturers created')
     if settings.NETBOX_FEATURES['modules']:
-        settings.handle.log(f'{netbox.counter["module_added"]} modules created')
-        settings.handle.log(f'{netbox.counter["module_port_added"]} module interface / ports created')
+        settings.handle.log(
+            f'{netbox.counter["module_added"]} modules created')
+        settings.handle.log(
+            f'{netbox.counter["module_port_added"]} module interface / ports created')
+
 
 if __name__ == "__main__":
     main()
